9e7163acdf24b78d36271865c1eafb74c2f9249a
Arinze Umenyira <u.arinze@yahoo.com>
2018-03-20 11:48:23 -0400

    First website upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var express =  require('express');
+var path = require('path');
+var app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('views'));
+app.use('/images' , express.static(path.join(__dirname,'images')))
+/*app.use('/plugin' , express.static(path.join(__dirname,'plugin')));
+app.use('/js' , express.static(path.join(__dirname,'js'))); */
+
+app.get('/', function(req, res)
+{
+  res.render('home');
+});
+
+app.listen(3000, function(){
+  console.log("were listeing");
+});
